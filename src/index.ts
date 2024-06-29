7da0dc5a49af67d0f3584c56ae4502a6bcccecfa
Nikita Grichan <anivire.work@gmail.com>
2024-06-29 08:26:44 +0300

    fix: add twitter logger

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import {
   cronFetchArtistSuggestion,
   cronUpdateStats,
 } from './services/cronService';
+import { test } from './services/twitterService';
 import { logger } from './utils';
 
 logger('Service started.');
+// test();
 // sendDiscordMessage('Service started', 'dotcreators worker service is online!');
 cronUpdateStats();
 cronFetchArtistSuggestion();
--- src/services/twitterService.ts
+++ src/services/twitterService.ts
@@ -4,33 +4,46 @@ import { getOriginalUrl } from '../utils';
 export class TwitterService {
   private readonly scraper = new Scraper();
 
-  async getTwitterProfile(username: string): Promise<Profile> {
-    let profile = await this.scraper.getProfile(username);
+  async getTwitterProfile(username: string): Promise<Profile | undefined> {
+    try {
+      let profile = await this.scraper.getProfile(username);
+      if (profile.biography) {
+        const regex =
+          /https?:\/\/(?:www\.|(?!www))[^\s.]+(?:\.[^\s.]+)+(?:\w\/?)*/gi;
 
-    if (profile.biography) {
-      const regex =
-        /https?:\/\/(?:www\.|(?!www))[^\s.]+(?:\.[^\s.]+)+(?:\w\/?)*/gi;
+        const matches = profile.biography.match(regex);
 
-      const matches = profile.biography.match(regex);
+        if (matches) {
+          const promises = matches.map(async link => {
+            const originalUrl = await getOriginalUrl(link);
+            return originalUrl || '';
+          });
 
-      if (matches) {
-        const promises = matches.map(async link => {
-          const originalUrl = await getOriginalUrl(link);
-          return originalUrl || '';
-        });
+          const newBioArray = await Promise.all(promises);
 
-        const newBioArray = await Promise.all(promises);
+          let index = 0;
+          const processedBio = profile.biography.replace(
+            regex,
+            () => newBioArray[index++] || ''
+          );
 
-        let index = 0;
-        const processedBio = profile.biography.replace(
-          regex,
-          () => newBioArray[index++] || ''
-        );
-
-        profile.biography = processedBio;
+          profile.biography = processedBio;
+        }
       }
-    }
 
-    return profile;
+      return profile;
+    } catch (e) {
+      console.log(e);
+      return undefined;
+    }
+  }
+}
+
+export async function test() {
+  const t = new TwitterService();
+  try {
+    console.log(await t.getTwitterProfile('MortMort_'));
+  } catch (e) {
+    console.log(e);
   }
 }
